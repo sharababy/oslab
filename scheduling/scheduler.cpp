2102b14357c9d0e15904fd0b93e5a361c4a1a9b9
iiitdm <bassi.vdt@gmail.com>
2017-08-01 22:15:21 +0530

    sjf:computing min

---
+++ scheduling/scheduler.cpp
@@ -0,0 +1,88 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int ct[5], tat[5], wt[5];
+
+
+int compare(const void * a , const void * b){
+
+	return (*(int*)a - *(int*)b );
+
+}
+
+int sjf_gen(int pid[] , int at[] , int bt[]){
+	
+	
+		
+	int min=-1;	
+
+	qsort(at,5,sizeof(int),compare);
+
+	ct[0]=at[0]+bt[0];
+	tat[0]=ct[0]-at[0];
+	wt[0]=tat[0]-bt[0];
+
+	int last = 0,index=0;
+
+		int pre[5]={100,100,100,100,100},u=0;
+		for(int k=1 ; k<5 ; k++){
+			
+			if(ct[last] >= at[k]){
+				pre[k] = bt[k];
+				u++; 				
+			}
+		}
+	for (int i = 1; i < 5; ++i)
+	{
+		int min = 100;
+
+ 		if(u==0){
+			printf("\n\nNo more processes!\n\n");
+			break;
+		}
+		else{
+			for(int k=0 ; k<5 ; k++){
+		//		printf(" %d ",pre[k]);
+			}			
+
+			for(int k =0; k<5 ; k++){
+				if(min > pre[k]){	
+					min = pre[k];
+					index=k;
+					pre[k] = 100;
+				}
+			}			
+
+				if(min!=100){
+				//	printf("\n\nct[last]: %d , min: %d ,index: %d , last: %d \n\n",ct[last],min,index,last);
+					ct[index]=ct[last]+ min;
+					tat[index]=ct[index]-at[index];
+					wt[index]=tat[index]-bt[index];
+					last = index;
+
+
+				}
+				
+		}
+	}
+	printf("\nShortest Job First Processing Schedule: ");
+	printf("\n--------------------------------------");
+	
+	
+		for (int i = 0; i < 5; ++i)
+		{
+			printf("\nProcess ID: %d", pid[i]);
+			printf("\tArrival Time: %d", at[i]);
+			printf("\tBurst Time: %d", bt[i]);
+			printf("\tCompletion Time: %d", ct[i]);
+			printf("\tTurn-around Time: %d", tat[i]);
+			printf("\tWaiting Time: %d", wt[i]);
+		}
+	
+	printf("\n");
+
+
+
+}
--- scheduling/scheduler.h
+++ scheduling/scheduler.h
@@ -1,3 +1,5 @@
-#include <stdio.h>
 
-int pid[5], at[5], bt[5], ct[5], tat[5], wt[5];
+
+
+	int compare(const void * a , const void * b);
+	int sjf_gen(int pid[] , int at[] , int bt[]);
