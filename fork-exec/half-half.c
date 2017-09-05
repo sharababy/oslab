6942c866ed66617369bb95745541f2e447fab680
bassi.vdt@gmail.com <bassi.vdt@gmail.com>
2017-09-05 15:24:54 +0530

    half half done

--- fork-exec/half-half.c
+++ fork-exec/half-half.c
@@ -1,20 +1,28 @@
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-
+#define size 10
 
 int main(){
+	
+	int m[size] = {7,3,9,4,132,67,12,75,44,49};
+	int half = size/2;
+	int k =0;
+	printf("original: ");	
+		for(k=0;k<size;k++){
 
-	int m[10] = {7,3,9,4,132,67,12,75,44,49};
-
+			printf(" %d ", m[k]);
+		}
+		printf("\n\n");
 	pid_t pid = fork();
 	
 	if(pid==0){
 		printf("\n-- We are in the child\n");
 		int i = 0,j=0;
 		int max;
-		for(i=0 ; i < 10; i++){
-			for(j=i ; j < 10; j++){
+		for(i=0 ; i < half; i++){
+			for(j=i ; j < half; j++){
 				
 				if(m[i] <= m[j]){
 					int temp = m[i];
@@ -26,7 +34,7 @@ int main(){
 
 
 		}
-		for(i=0;i<10;i++){
+		for(i=0;i<size;i++){
 
 			printf(" %d ", m[i]);
 		}
@@ -38,8 +46,8 @@ int main(){
 
 		int i = 0,j=0;
 		int max;
-		for(i=0 ; i < 10; i++){
-			for(j=i ; j < 10; j++){
+		for(i=half ; i < size; i++){
+			for(j=i ; j < size; j++){
 				
 				if(m[i] >= m[j]){
 					int temp = m[i];
@@ -51,7 +59,7 @@ int main(){
 
 
 		}
-		for(i=0;i<10;i++){
+		for(i=0;i<size;i++){
 
 			printf(" %d ", m[i]);
 		}
