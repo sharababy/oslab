04dc9553b2518fa918ad72b659ffba7e545239d7
Vedant Bassi <bassi.vdt@gmail.com>
2017-10-11 12:17:23 +0530

    dummy

--- threading/quicksort_t.c
+++ threading/quicksort_t.c
@@ -13,7 +13,7 @@ typedef struct Set{
 } set;
 
 
-// A utility function to swap two elements
+
 void swap(int* a, int* b)
 {
     int t = *a;
@@ -21,11 +21,7 @@ void swap(int* a, int* b)
     *b = t;
 }
  
-/* This function takes last element as pivot, places
-   the pivot element at its correct position in sorted
-    array, and places all smaller (smaller than pivot)
-   to left of pivot and all greater elements to right
-   of pivot */
+
 int partition (int arr[], int low, int high)
 {
     int pivot = arr[high];    // pivot
