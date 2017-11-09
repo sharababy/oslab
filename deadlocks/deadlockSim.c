662b935bc30e9202f0bbce082a6f818d26d70e49
Vedant Bassi <bassi.vdt@gmail.com>
2017-11-09 14:58:50 +0530

    Deadlock Simulation Done

---
+++ deadlocks/deadlockSim.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+
+#define SNAME "/commonMutex1"
+#define SEM_NAME "/commonMutex2"
+
+int main()
+{
+	
+	sem_t *mutex1 = sem_open(SNAME, O_CREAT, 0644, 1); 
+
+	printf("Created Mut 1\n");
+
+	sleep(2);
+
+
+	sem_t *mutex2 = sem_open(SEM_NAME, 0); /* Open a preexisting semaphore. */
+
+	printf("Opened Mut 2\n");
+
+	sem_wait(mutex1);
+
+	sleep(2);
+
+	printf("Going to Accquire Mut 2\n");
+	sem_wait(mutex2);
+
+	printf("Done\n");
+	sleep(2);
+
+	sem_post(mutex2);
+	sem_post(mutex1);
+
+
+	return 0;
+}
+
+
